b535ca00532bcec90191f46db3563f426e6e93a1
FirasMyacoup <firas.yacoup1@gmail.com>
2022-09-19 23:47:41 +0300

    lab07

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
 'use strict';
 
-const express = require ('express');
+const express = require( 'express' );
+const cors = require( 'cors' );
 const app = express();
-const cors = require('cors');
-const notFound = require('../401-whiteboard-backend/error-handlers.js/404');
-const errorHandler = require('../401-whiteboard-backend/error-handlers.js/500');
-const post = require('../401-whiteboard-backend/routes/post.routes')
+const NotFound = require( './error-handlers/404' );
+const error = require( './error-handlers/500' );
+const postRouter = require( './routes/post.route' );
+const commentRoute = require( './routes/comment-route' );
+const postRoute = require( './routes/post.route' );
+const userRoute = require('./routes/user.route');
 
-app.use(cors());
-app.use(post);
-app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).send("this is the Home page")
-});
+app.use( cors() );
+app.use( express.json() );
+app.use( postRouter );
+app.use( NotFound );
+app.use( error );
+app.use(commentRoute);
+app.use(postRoute);
+app.use( userRoute );
 
-app.use(notFound);
-app.use(errorHandler);
 
-function start(port) {
-    app.listen(port, () => console.log(`Welcome ${port}`));
+app.get( '/', ( req, res ) => {
+    res.status( 200 ).json( {
+        message: 'Home page',
+        code: 200
+    } );
+} );
+
+
+function start ( port ) {
+    app.listen( port, () => console.log( `Working : ${port}` ) );
 }
-
 module.exports = {
-    start,app
+    start,
+    app
+    
 };
